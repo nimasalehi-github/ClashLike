8711fc43e3549c0f735a450d870cde42c85814fa
n salehi <nimasalehi.auth@icloud.com>
2025-10-23 17:44:25 +0330

    Initial Commit

---
+++ ClashLike/ClashLikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClashLikeApp.swift
+//  ClashLike
+//
+//  Created by Nima Salehi on 10/23/25.
+//
+
+import SwiftUI
+
+@main
+struct ClashLikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
